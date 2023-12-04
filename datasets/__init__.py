fcb6af766f54139edaab3dcd7e3d0c8eb8116615
BlueDruddigon <haibuihoang1999@gmail.com>
2023-12-04 16:44:39 +0700

    Refactor: Code Refactor

--- datasets/__init__.py
+++ datasets/__init__.py
@@ -9,8 +9,10 @@ from .transforms import get_default_transforms
 
 def build_dataset(args: argparse.Namespace):
     transforms = get_default_transforms(args)
-    train_set = AbdomenDataset(args.data_root, transform=transforms, is_train=args.train, num_workers=args.workers),
-    valid_set = AbdomenDataset(args.data_root, transform=transforms, is_train=not args.train, num_workers=args.workers)
+    train_set = AbdomenDataset(args.data_root, transform=transforms, is_train=args.is_train, num_workers=args.workers),
+    valid_set = AbdomenDataset(
+      args.data_root, transform=transforms, is_train=not args.is_train, num_workers=args.workers
+    )
     
     # data sampler and dataloader
     train_sampler = CustomSampler(train_set) if args.distributed else None
